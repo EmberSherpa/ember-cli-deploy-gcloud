a04abc6dc11e1afcb47ccd3d3129d3f6f486a6de
Taras Mankovski <tarasm@gmail.com>
2016-05-12 21:41:40 -0400

    Deployment process works

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+const GoogleCloudDeployPlugin = require('./lib/google-cloud-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-gcloud'
+  name: 'ember-cli-deploy-gcloud',
+  
+  createDeployPlugin: function(options) {
+    return new GoogleCloudDeployPlugin({
+      name: options.name
+    });
+  }
 };
